2e9ba0cfba760d6cce986242c67e9a905f7717d2
C1281149 Lucas Dynczuki Palla <c1281149@interno.bb.com.br>
2021-04-07 20:20:48 -0300

    Ajustes região aws configure

--- code/terraform/main.tf
+++ code/terraform/main.tf
@@ -9,9 +9,6 @@ terraform {
 
 # Retirar as credenciais antes de subir pro git
 provider "aws" {
-    # region = "sa-east-1"
     region = "us-east-2"
-    # access_key = "AKIA3RRF77IFT2ETWOPC"
-    # secret_key = "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"
     shared_credentials_file = pathexpand("~/.aws/credentials")
 }
